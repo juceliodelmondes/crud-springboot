02fc70788069986ca5e08c017df8c3a49026cea2
Diana Regina <devdianaregina@gmail.com>
2024-09-07 14:21:12 -0300

    estrutura basica do crud

---
+++ src/main/java/br/com/gestaoalunos/crud/CrudSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaoalunos.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gestaoalunos/crud/CrudSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaoalunos.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
